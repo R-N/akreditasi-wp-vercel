df1c26c46bc8714a8d6158197956c53994f5a1ee
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-11-20 21:28:07 +0700

    search

---
+++ wp-content/plugins/wp-static-search/wp-static-search.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html><body><br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_action() in E:\Downloads\akreditasi-wp-laragon\www\akreditasi\wp-content\plugins\wp-static-search\wp-static-search.php:16
+Stack trace:
+#0 {main}
+  thrown in <b>E:\Downloads\akreditasi-wp-laragon\www\akreditasi\wp-content\plugins\wp-static-search\wp-static-search.php</b> on line <b>16</b><br>
+</body></html>
+<!-- Powered by Staatic (https://staatic.com/) -->
